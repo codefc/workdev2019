1ceaa528e22ebda0b657f1845c5069500c46e0fe
azure_devops_codecommit-at-735882332916 <albert.tanure@kapturedocs.com.br>
2019-04-27 16:17:05 -0300

    Segundo commit

---
+++ src/WorkDev/WorkDev/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WorkDev.Models;
+
+namespace WorkDev.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
